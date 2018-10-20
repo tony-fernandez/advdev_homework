59dd61b9c6368e215d8cce11e6efbdd4156c4890
Tony Fernandez <tony.fernandez.extern@pyur.com>
2018-10-20 13:36:17 +0200

    Fix nexus

--- Infrastructure/bin/setup_nexus.sh
+++ Infrastructure/bin/setup_nexus.sh
@@ -7,31 +7,11 @@ if [ "$#" -ne 1 ]; then
 fi
 
 GUID=$1
-echo "Setting up Nexus in project $GUID-nexus"
-
-# Code to set up the Nexus. It will need to
-# * Create Nexus
-# * Set the right options for the Nexus Deployment Config
-# * Load Nexus with the right repos
-# * Configure Nexus as a docker registry
-# Hint: Make sure to wait until Nexus if fully up and running
-#       before configuring nexus with repositories.
-#       You could use the following code:
-# while : ; do
-#   echo "Checking if Nexus is Ready..."
-#   oc get pod -n ${GUID}-nexus|grep '\-2\-'|grep -v deploy|grep "1/1"
-#   [[ "$?" == "1" ]] || break
-#   echo "...no. Sleeping 10 seconds."
-#   sleep 10
-# done
-
-# Ideally just calls a template
-# oc new-app -f ../templates/nexus.yaml --param .....
-
-# To be Implemented by Student
+NEXUS=$GUID-nexus
+echo "Setting up Nexus in project ${NEXUS}"
 
 # Change to the correct project
-oc project ${GUID}-nexus
+oc project ${NEXUS}
 
 oc new-app sonatype/nexus3:latest
 oc expose svc nexus3
@@ -41,7 +21,6 @@ oc set resources dc nexus3 --limits=memory=2Gi,cpu=2 --requests=memory=1Gi,cpu=5
 
 # Create persistent volume mount
 oc create -f ../templates/nexus-pvc.yaml
-
 
 oc set volume dc/nexus3 --add --overwrite --name=nexus3-volume-1 --mount-path=/nexus-data/ --type persistentVolumeClaim --claim-name=nexus-pvc
 
@@ -53,12 +32,12 @@ http_status=""
 while : ; do  
   echo "Checking if Nexus is up."
   #http_status=$(curl -I http://nexus3-tf-nexus.127.0.0.1.nip.io/repository/maven-public/ -o /dev/null -w '%{http_code}\n' -s)
-  #http://$(oc get route nexus3 --template='{{ .spec.host }}' -n ${GUID}-nexus) 
-  http_status=$(curl -I http://$(oc get route nexus3 --template='{{ .spec.host }}' -n ${GUID}-nexus)/repository/maven-public/ -o /dev/null -w '%{http_code}\n' -s)
+  #http://$(oc get route nexus3 --template='{{ .spec.host }}' -n ${NEXUS}) 
+  http_status=$(curl -I http://$(oc get route nexus3 --template='{{ .spec.host }}' -n ${NEXUS})/repository/maven-public/ -o /dev/null -w '%{http_code}\n' -s)
   echo "Http call returned code: ${http_status}"	
   [[ "$http_status" != "200" ]] || break
-  echo "...no. Sleeping 10 seconds."    
-  sleep 10
+  echo "Sleeping 10 seconds...."    
+  sleep 20
 done
 
 curl -o setup_nexus3.sh -s https://raw.githubusercontent.com/wkulhanek/ocp_advanced_development_resources/master/nexus/setup_nexus3.sh
@@ -66,3 +45,12 @@ chmod +x setup_nexus3.sh
 ./setup_nexus3.sh admin admin123 http://$(oc get route nexus3 --template='{{ .spec.host }}')
 rm setup_nexus3.sh
 
+oc expose dc nexus3 --port=5000 --name=nexus-registry -n ${NEXUS}
+oc create route edge nexus-registry --service=nexus-registry --port=5000 -n ${NEXUS}
+
+oc get routes -n ${NEXUS}
+
+# In order to display the regular Nexus route add an annotation to the Nexus route 
+# (this only works with OpenShift 3.10 and newer)
+oc annotate route nexus3 console.alpha.openshift.io/overview-app-route=true -n ${NEXUS}
+oc annotate route nexus-registry console.alpha.openshift.io/overview-app-route=false -n ${NEXUS}
