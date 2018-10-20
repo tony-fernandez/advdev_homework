0b61650f437373e0084fc88dd05ffad083a00d05
Tony Fernandez <tony.fernandez.extern@pyur.com>
2018-10-20 08:48:27 +0200

    Add dev configuration

--- Infrastructure/bin/setup_dev.sh
+++ Infrastructure/bin/setup_dev.sh
@@ -7,8 +7,55 @@ if [ "$#" -ne 1 ]; then
 fi
 
 GUID=$1
-echo "Setting up Parks Development Environment in project ${GUID}-parks-dev"
+PARKS_DEV=${GUID}-parks-dev
+echo "Setting up Parks Development Environment in project ${PARKS_DEV}"
+# Switch to dev
+oc project ${PARKS_DEV}
 
-# Code to set up the parks development project.
+# Grant the correct permissions to the Jenkins service account
+oc policy add-role-to-user view --serviceaccount=default -n ${PARKS_DEV}
+oc policy add-role-to-user edit system:serviceaccount:$GUID-jenkins:jenkins -n ${PARKS_DEV}
+oc policy add-role-to-user admin system:serviceaccount:gpte-jenkins:jenkins -n ${PARKS_DEV}
+# Create a MongoDB database
+oc new-app --template=mongodb-persistent --param=MONGODB_USER=mongodb --param=MONGODB_PASSWORD=mongodb --param=MONGODB_DATABASE=parks -n ${PARKS_DEV}
+# Create binary build configurations for the pipelines to use for each microservice
+oc new-build --binary=true --name="mlbparks" jboss-eap70-openshift:1.7 -n ${PARKS_DEV}
+oc new-build --binary=true --name="nationalparks" redhat-openjdk18-openshift:1.2 -n ${PARKS_DEV}
+oc new-build --binary=true --name="parksmap" redhat-openjdk18-openshift:1.2 -n ${PARKS_DEV}
+# Create ConfigMaps for configuration of the application
+oc create configmap mlbparks-dev-config --from-env-file=../templates/mlbparks-dev.env -n ${PARKS_DEV}
+oc create configmap nationalparks-dev-config --from-env-file=../templates/nationalparks-dev.env -n ${PARKS_DEV}
+oc create configmap parksmap-dev-config --from-env-file=../templates/parksmap-dev.env -n ${PARKS_DEV}
+# Set up placeholder deployment configurations for the three microservices
+oc new-app ${PARKS_DEV}/mlbparks:0.0-0 --name=mlbparks --allow-missing-imagestream-tags=true -n ${PARKS_DEV}
+oc new-app ${PARKS_DEV}/nationalparks:0.0-0 --name=nationalparks --allow-missing-imagestream-tags=true -n ${PARKS_DEV}
+oc new-app ${PARKS_DEV}/parksmap:0.0-0 --name=parksmap --allow-missing-imagestream-tags=true -n ${PARKS_DEV}
+oc set triggers dc/mlbparks --remove-all -n ${PARKS_DEV}
+oc set triggers dc/nationalparks --remove-all -n ${PARKS_DEV}
+oc set triggers dc/parksmap --remove-all -n ${PARKS_DEV}
+# Configure the deployment configurations using the ConfigMaps
+oc set env dc/mlbparks --from=configmap/mlbparks-config -n ${PARKS_DEV}
+oc set env dc/nationalparks --from=configmap/nationalparks-config -n ${PARKS_DEV}
+oc set env dc/parksmap --from=configmap/parksmap-config -n ${PARKS_DEV}
 
-# To be Implemented by Student
+oc expose dc mlbparks --port 8080 -n ${PARKS_DEV}
+oc expose dc nationalparks --port 8080 -n ${PARKS_DEV}
+oc expose dc parksmap --port 8080 -n ${PARKS_DEV}
+
+# set deployment hooks to call /ws/data/load/ and populate the database for the back end services
+oc set deployment-hook dc/mlbparks  -n ${PARKS_DEV} --post -c mlbparks --failure-policy=abort -- curl http://$(oc get route mlbparks -n ${PARKS_DEV} -o jsonpath='{ .spec.host }')/ws/data/load/
+oc set deployment-hook dc/nationalparks  -n ${PARKS_DEV} --post -c nationalparks --failure-policy=abort -- curl http://$(oc get route nationalparks -n ${PARKS_DEV} -o jsonpath='{ .spec.host }')/ws/data/load/
+oc set deployment-hook dc/parksmap  -n ${PARKS_DEV} --post -c parksmap --failure-policy=abort -- curl http://$(oc get route parksmap -n ${PARKS_DEV} -o jsonpath='{ .spec.host }')/ws/data/load/
+
+# Set up liveness and readiness probes
+oc set probe dc/mlbparks -n ${PARKS_DEV} --liveness --failure-threshold 5 --initial-delay-seconds 30 -- echo ok
+oc set probe dc/mlbparks --readiness --failure-threshold 3 --initial-delay-seconds 60 --get-url=http://:8080/ws/healthz/ -n ${PARKS_DEV}
+oc set probe dc/nationalparks -n ${PARKS_DEV} --liveness --failure-threshold 5 --initial-delay-seconds 30 -- echo ok
+oc set probe dc/nationalparks --readiness --failure-threshold 3 --initial-delay-seconds 60 --get-url=http://:8080/ws/healthz/ -n ${PARKS_DEV}
+oc set probe dc/parksmap -n ${PARKS_DEV} --liveness --failure-threshold 5 --initial-delay-seconds 30 -- echo ok
+oc set probe dc/parksmap --readiness --failure-threshold 5 --initial-delay-seconds 60 --get-url=http://:8080/ws/healthz/ -n ${PARKS_DEV}
+
+# Expose and label the services properly (parksmap-backend)
+oc expose svc mlbparks -n ${PARKS_DEV} --labels="type=parksmap-backend"
+oc expose svc nationalparks -n ${PARKS_DEV} --labels="type=parksmap-backend"
+oc expose svc parksmap -n ${PARKS_DEV}
--- Infrastructure/bin/setup_sonar.sh
+++ Infrastructure/bin/setup_sonar.sh
@@ -10,15 +10,33 @@ GUID=$1
 SONAR_PROJECT_NAME=$GUID-sonarqube
 echo "Setting up Sonarqube in project ${SONAR_PROJECT_NAME}"
 
-# Code to set up the SonarQube project.
-# Ideally just calls a template
-# oc new-app -f ../templates/sonarqube.yaml --param .....
-
 # Switch to Sonarqube project.
+echo "Switching to ${SONAR_PROJECT_NAME} project"
 oc project ${SONAR_PROJECT_NAME}
+
 # Setup postgress db
+echo "Setting up postgress database..."
 oc new-app --template=postgresql-persistent --param POSTGRESQL_USER=sonar --param POSTGRESQL_PASSWORD=sonar --param POSTGRESQL_DATABASE=sonar --param VOLUME_CAPACITY=4Gi --labels=app=sonarqube_db 
+
 # Deploy SonarQube
+echo "Creating new sonaqube app..."
 oc new-app --docker-image=wkulhanek/sonarqube:6.7.4 --env=SONARQUBE_JDBC_USERNAME=sonar --env=SONARQUBE_JDBC_PASSWORD=sonar --env=SONARQUBE_JDBC_URL=jdbc:postgresql://postgresql/sonar --labels=app=sonarqube
 oc rollout pause dc sonarqube
 oc expose service sonarqube
+
+# Create persistent volume claim and set it to sonarqube
+echo "Creating persistent volume claim..."
+oc create -f ../templates/sonarqube-pvc.yaml
+oc set volume dc/sonarqube --add --overwrite --name=sonarqube-volume-1 --mount-path=/opt/sonarqube/data/ --type persistentVolumeClaim --claim-name=sonarqube-pvc
+
+# Set resources
+echo "Setting resources..."
+oc set resources dc/sonarqube --limits=memory=3Gi,cpu=2 --requests=memory=2Gi,cpu=1
+oc patch dc sonarqube --patch='{ "spec": { "strategy": { "type": "Recreate" }}}'
+
+# Add liveliness and readiness probes
+echo "Adding liveliness and readinees probes..."
+oc set probe dc/sonarqube --liveness --failure-threshold 3 --initial-delay-seconds 40 -- echo ok
+oc set probe dc/sonarqube --readiness --failure-threshold 3 --initial-delay-seconds 20 --get-url=http://:9000/about
+oc rollout resume dc sonarqube
+oc rollout status dc/sonarqube --watch -n ${SONAR_PROJECT_NAME}
