35acabf060e2b09ed54bd46d78551ba9ce294608
Tony Fernandez <tony.fernandez.extern@pyur.com>
2018-10-21 19:05:18 +0200

    Update nexus setup

--- Infrastructure/bin/setup_nexus.sh
+++ Infrastructure/bin/setup_nexus.sh
@@ -70,7 +70,7 @@ done
 
 curl -o setup_nexus3.sh -s https://raw.githubusercontent.com/wkulhanek/ocp_advanced_development_resources/master/nexus/setup_nexus3.sh
 chmod +x setup_nexus3.sh
-./setup_nexus3.sh admin admin123 http://$(oc get route nexus3 --template='{{ .spec.host }}')
+./setup_nexus3.sh admin admin123 http://$(oc get route nexus3 -n ${NEXUS} --template='{{ .spec.host }}')
 rm setup_nexus3.sh
 
 oc expose dc nexus3 --port=5000 --name=nexus-registry -n ${NEXUS}
